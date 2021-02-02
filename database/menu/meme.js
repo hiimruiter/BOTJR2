466a6c6f62180e9da691812e5394d806df9012fd
hiimruiter <78325187+hiimruiter@users.noreply.github.com>
2021-02-02 12:18:35 -0300

    Add files via upload

---
+++ database/menu/meme.js
@@ -0,0 +1,20 @@
+const meme = (prefix, botName, ownerName) => {
+	return `
+「 *JUNIN😈* 」
+
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: JUNIN
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo`
+}
+exports.meme = meme
